3d8fa84d00643ada9b1709ede7909c1884aef9ee
Marcin Skrzypkowski <m.skrzypkowski@stud.elka.pw.edu.pl>
2024-10-17 22:25:42 +0000

    distribute calculations among more blocks, add options for correct debug for cuda-gdb

--- GPU/add.cu
+++ GPU/add.cu
@@ -5,8 +5,8 @@
 __global__
 void add(int n, float *x, float *y)
 {
-  int index = threadIdx.x;
-  int stride = blockDim.x;
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x *gridDim.x;
   for (int i = index; i < n; i += stride)
       y[i] = x[i] + y[i];
 }
