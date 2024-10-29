c2101b2f7f6719d7c209e9d06776af43c7de1bda
Marcin Skrzypkowski <m.skrzypkowski@stud.elka.pw.edu.pl>
2024-10-29 20:49:28 +0000

    Use Vec3 in main.cpp

--- CPP/rt-in-one-weekend/main.cpp
+++ CPP/rt-in-one-weekend/main.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <filesystem>
+
+#include "Vec3.h"
 
 int main () {
     int nx = 200;
     int ny = 100;
     std::ofstream simple_ppm;
-    simple_ppm.open("/home/cuda_ws/CPP/rt-in-one-weekend/artifacts/simple.ppm");
+    const auto file_path = std::filesystem::current_path() / "../CPP/rt-in-one-weekend/artifacts/simple.ppm";
+    simple_ppm.open(file_path);
     simple_ppm << "P3\n" <<nx << " " << ny << "\n255\n";
     for (int j = ny - 1; j >= 0; j--)
     {
         for (int i = 0; i < nx; i++)
         {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            Vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+            int ir = int(255.99 * col.r());
+            int ig = int(255.99 * col.g());
+            int ib = int(255.99 * col.b());
             simple_ppm << ir << " " << ig << " " << ib << "\n";
-
         }
     }
-
     simple_ppm.close();
     return 0;
 }
