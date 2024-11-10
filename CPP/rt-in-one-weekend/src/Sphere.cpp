3672b2348fa9badb8edae45a8a32d0d4e09172fd
Marcin Skrzypkowski <m.skrzypkowski@stud.elka.pw.edu.pl>
2024-11-10 18:48:35 +0000

    follow the script

--- CPP/rt-in-one-weekend/src/Sphere.cpp
+++ CPP/rt-in-one-weekend/src/Sphere.cpp
@@ -8,13 +8,13 @@ bool Sphere::hit(const Ray &ray, const float t_min, const float t_max, HitRecord
     const Vec3 circle_origin = ray.origin() - center_;  // what is this?
     // parameters of quadratic equation at^2 + bt + c (notes chapter 4)
     const float a = RT::dot(ray.direction(), ray.direction());
-    const float b = 2.0 * RT::dot(circle_origin, ray.direction());
+    const float b = RT::dot(circle_origin, ray.direction());
     const float c = RT::dot(circle_origin, circle_origin) - radius_ * radius_;
-    const float delta = b * b - 4 * a * c;
+    const float delta = b * b - a * c;
 
     if (delta > 0)
     {
-        float temp = (-b - sqrt(b*b - a * c)) / (2.0 * a);  // closer point hitting the sphere
+        float temp = (-b - sqrt(b*b - a * c)) / a;  // closer point hitting the sphere
         if (temp < t_max && temp > t_min)
         {
             record.t = temp;
@@ -22,7 +22,7 @@ bool Sphere::hit(const Ray &ray, const float t_min, const float t_max, HitRecord
             record.normal = (record.point - center_) / radius_;
             return true;
         }
-        temp = (-b + sqrt(b*b - a * c)) / (2.0 * a);  // farther point hitting the sphere
+        temp = (-b + sqrt(b*b - a * c)) / a;  // farther point hitting the sphere
         if (temp < t_max && temp > t_min)
         {
             record.t = temp;
