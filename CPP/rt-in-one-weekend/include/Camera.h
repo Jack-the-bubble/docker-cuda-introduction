47fecf915fe60ac5e3aa671b7855eb58cc3f6a21
Marcin Skrzypkowski <m.skrzypkowski@stud.elka.pw.edu.pl>
2024-11-10 20:47:07 +0000

    add chapter six, also notes for chapter 4, 5 and 6

---
+++ CPP/rt-in-one-weekend/include/Camera.h
@@ -0,0 +1,28 @@
+#ifndef RT_CAMERA_H
+#define RT_CAMERA_H
+
+#include "Ray.h"
+
+namespace RT {
+
+class Camera {
+public:
+    Camera(const Vec3 &low_left_corner, const Vec3 &hor, const Vec3 &ver, const Vec3 &origin):
+            lower_left_corner_(low_left_corner), horizontal_(hor), vertical_(ver), origin_(origin)
+    {}
+    Camera() = default;
+    ~Camera() = default;
+
+    Ray get_ray(const float u, const float v)
+    {
+        return Ray(origin_, lower_left_corner_ + u*horizontal_ + v*vertical_ - origin_);
+    }
+private:
+    Vec3 origin_;
+    Vec3 lower_left_corner_;
+    Vec3 horizontal_;
+    Vec3 vertical_;
+};
+} // namespace RT
+
+#endif // RT_CAMERA_H
