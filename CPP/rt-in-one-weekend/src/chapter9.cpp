0e5e289e093c0560f61bc584a6c8032e1116ee77
Marcin Skrzypkowski <m.skrzypkowski@stud.elka.pw.edu.pl>
2024-11-11 15:49:39 +0000

    all spheres have positive radius

--- CPP/rt-in-one-weekend/src/chapter9.cpp
+++ CPP/rt-in-one-weekend/src/chapter9.cpp
@@ -30,7 +30,7 @@ int main() {
     list[0] = new RT::Sphere(RT::Vec3(0, 0, -1), 0.5, lambertian_1);
     list[1] = new RT::Sphere(RT::Vec3(0, -100.5, -1), 100, lambertian_2);
     list[2] = new RT::Sphere(RT::Vec3(1, 0, -1), 0.5, metal_1);
-    list[3] = new RT::Sphere(RT::Vec3(-1, 0, -1), -0.5, dielectric);
+    list[3] = new RT::Sphere(RT::Vec3(-1, 0, -1), 0.5, dielectric);
     RT::Hitable *world = new RT::HitableList(list, 4);
     RT::Camera cam({-2.0, -1.0, -1.0}, {4.0, 0.0, 0.0}, {0.0, 2.0, 0.0}, {0.0, 0.0, 0.0});
 
