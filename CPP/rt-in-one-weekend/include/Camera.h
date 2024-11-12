bf27ae57f75e6a49033a3373109b267470aaca99
Marcin Skrzypkowski <m.skrzypkowski@stud.elka.pw.edu.pl>
2024-11-12 18:01:55 +0000

    chapter-10

--- CPP/rt-in-one-weekend/include/Camera.h
+++ CPP/rt-in-one-weekend/include/Camera.h
@@ -7,6 +7,30 @@ namespace RT {
 
 class Camera {
 public:
+    Camera(const Vec3 &lookfrom, const Vec3 &lookat, Vec3 &vector_up, const float vertical_fov, const float aspect_ratio): origin_(lookfrom)
+    {
+        Vec3 u, v, w;
+        const float theta = vertical_fov * M_PI / 180;
+        const float half_height = tan(theta / 2);  // from proportions
+        const float half_width = aspect_ratio * half_height;
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vector_up, w));
+        v = cross(w, u);
+        lower_left_corner_ = origin_ - half_width * u = half_height * v - w;
+        horizontal_ = 2 * half_width * u;
+        vertical_ = 2 * half_height * v;
+    }
+
+    Camera(const float vertical_fov, const float aspect_ratio, const Vec3 &origin): origin_(origin)
+    {
+        const float theta = vertical_fov * M_PI / 180;
+        const float half_height = tan(theta / 2);  // from proportions
+        const float half_width = aspect_ratio * half_height;
+        lower_left_corner_ = Vec3(-half_width, -half_height, -1.0);  // -1 in z coords
+        horizontal_ = Vec3(2 * half_width, 0, 0);
+        vertical_ = Vec3(0, 2 * half_height, 0);
+
+    }
     Camera(const Vec3 &low_left_corner, const Vec3 &hor, const Vec3 &ver, const Vec3 &origin):
             lower_left_corner_(low_left_corner), horizontal_(hor), vertical_(ver), origin_(origin)
     {}
---
+++ CPP/rt-in-one-weekend/src/chapter10.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include "Utils.h"
+#include "Sphere.h"
+#include "HitableList.h"
+#include "Lambertian.h"
+#include "Metal.h"
+#include "Dielectric.h"
+#include "Camera.h"
+
+int main() {
+   const int nx = 200;
+    int ny = nx / 2;
+    int ns = 100;  // antialiasing rays for a single pixel
+    RT::Vec3 lower_left_corner = {-2.0, -1.0, -1.0};
+    RT::Vec3 horizontal = {4.0, 0.0, 0.0};
+    RT::Vec3 vertical = {0.0, 2.0, 0.0};
+    RT::Vec3 origin = {0.0, 0.0, 0.0};
+    std::ofstream simple_ppm;
+    const auto file_path = std::filesystem::current_path() / "../CPP/rt-in-one-weekend/artifacts/chapter-10.ppm";
+    simple_ppm.open(file_path);
+    simple_ppm << "P3\n" <<nx << " " << ny << "\n255\n";
+
+    const float R = cos(M_PI / 4);
+    RT::Material *lambertian_1 = new RT::Lambertian({0, 0, 1});
+    RT::Material *lambertian_2 = new RT::Lambertian({1, 0, 0});
+    RT::Hitable *list[2];
+    list[0] = new RT::Sphere(RT::Vec3(-R, 0, -1), R, lambertian_1);
+    list[1] = new RT::Sphere(RT::Vec3(R, 0, -1), R, lambertian_2);
+    RT::Hitable *world = new RT::HitableList(list, 2);
+    RT::Camera cam(90, float(nx) / float(ny), {0, 0, 0});
+
+    for (int j = ny - 1; j >=0; j--) {
+        for (int i = 0; i < nx; i++) {
+            RT::Vec3 col(0, 0, 0);
+            for (int s = 0; s < ns; s++) {
+                // antialiasing step: kinda wasteful when we're doing it for the whole image and not just the edges
+                const float u = float(i + get_random()) / float(nx);
+                const float v = float(j + get_random()) / float(ny);
+                RT::Ray ray = cam.get_ray(u, v);
+                RT::Vec3 point = ray.point_at_parameter(2.0);  // distance from camera?
+                col += color(ray, world, 0);
+            }
+            col /= float(ns);
+            col = RT::Vec3(sqrt(col.r()), sqrt(col.g()), sqrt(col.b()));
+            const int ir = int(255.99 * col.r());
+            const int ig = int(255.99 * col.g());
+            const int ib = int(255.99 * col.b());
+            simple_ppm << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+    simple_ppm.close();
+
+    delete world;
+    delete list[0];
+    delete list[1];
+    delete lambertian_1;
+    delete lambertian_2;
+    return 0;
+}
